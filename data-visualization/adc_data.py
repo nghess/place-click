03f4483b365a81267893c47d0059f6cde0a41f90
Smear Lab <nhess@uoregon.edu>
2024-11-03 22:19:59 -0800

    added histogram

--- data-visualization/adc_data.py
+++ data-visualization/adc_data.py
@@ -3,22 +3,24 @@ import matplotlib.pyplot as plt
 import time
 
 # Import .dat and zip raw and smoothed into DF
-data = np.fromfile('data-visualization/sample-data/amplifierdata.bin', dtype=float)
+#data = np.fromfile('data-visualization/sample-data/amplifierdata.bin', dtype=float)
 
-#data = np.fromfile('data-visualization/sample-data/digitaldata.bin', dtype=float)
+data = np.fromfile('data-visualization/sample-data/digitaldata.bin', dtype=float)
 
 print(data.shape)
-window_end = 20000
+window_end = -1
 
 
-# plt.plot(data[1:window_end:8])
-# plt.plot(data[2:window_end:8])
-# plt.plot(data[3:window_end:8])
-plt.plot(data[8:-1:8])
+#plt.plot(data[1:window_end:8])
+#plt.plot(data[2:window_end:8])
+#plt.plot(data[3:window_end:8])
+plt.plot(data[1:window_end:3])
 # plt.plot(data[5:window_end:8])
 # plt.plot(data[6:window_end:8])
 # plt.plot(data[7:window_end:8])
+# plt.plot(data[8:window_end:8])
 
+#plt.plot(data)
 
 plt.show()
 
