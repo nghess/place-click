5d1fad89b282c33d194edac44abba5f573e6ed82
Smear Lab <nhess@uoregon.edu>
2024-11-11 08:50:37 -0800

    changed crop

--- data-visualization/adc_data.py
+++ data-visualization/adc_data.py
@@ -5,20 +5,15 @@ import time
 # Import .dat and zip raw and smoothed into DF
 #data = np.fromfile('data-visualization/sample-data/amplifierdata.bin', dtype=float)
 
-data = np.fromfile('data-visualization/sample-data/digitaldata.bin', dtype=float)
+data = np.fromfile('data-visualization/sample-data/daq.bin', dtype=float)
 
 print(data.shape)
-window_end = -1
+window_end = 5000
 
-
-#plt.plot(data[1:window_end:8])
-#plt.plot(data[2:window_end:8])
-#plt.plot(data[3:window_end:8])
-plt.plot(data[1:window_end:3])
-# plt.plot(data[5:window_end:8])
-# plt.plot(data[6:window_end:8])
-# plt.plot(data[7:window_end:8])
-# plt.plot(data[8:window_end:8])
+plt.plot(data[0:window_end:4])
+#plt.plot(data[1:window_end:4])
+#plt.plot(data[2:window_end:4])
+#plt.plot(data[3:window_end:4])
 
 #plt.plot(data)
 
